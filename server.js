18601cb806e7ad13d35f28a41ac5d195a77129dd
Kaaazmi <39811688+Kaaazmi@users.noreply.github.com>
2019-05-10 04:45:38 +0500

    databasechk2

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-trapezoidal-54357',
-      user : 'postgres',
-      password : '',
-      database : 'smartbrain'
+      connectionString : 'process.env.DATABASE_URL',
+      ssl: true,
     }
   });
 
